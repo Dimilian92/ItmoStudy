7cb071c0e65c2c5a59855f7dfafd92eac82b44e3
Dima <dimilian92@gmail.com>
2018-03-21 20:35:09 +0300

    ITMO commit1

---
+++ src/DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+    public  static void main (String[]args ){
+        System.out.println("Zhopa");
+
+        short a = 2;
+        short b = 7;
+        short c = (short)(a+b);
+
+        int a1 = 9;
+        int a2 = 4;
+        int b1 = a1/a2;
+        System.out.println(b1);
+
+        float q = 0.2f;
+        float e = 0.5f;
+        float w = q*e;
+        System.out.println(w);
+        int x =30;
+        while (x>20){
+            System.out.println("Davai novoi infi");
+            x--;
+        }
+    }
+}
