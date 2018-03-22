8dac7ab13e2c3471a9cf6901a2b4388947f588ad
Dima <dimilian92@gmail.com>
2018-03-22 23:17:43 +0300

    Очередные ночные посиделки

--- src/HW1.java
+++ src/HW1.java
@@ -1,4 +1,5 @@
 import static java.lang.Math.abs;
+import static java.lang.Math.sqrt;
 
 public class HW1 {
     public static void main(String[] args) {
@@ -11,8 +12,29 @@ public class HW1 {
             System.out.println("Ближайшее число: " + m);}
             else {
             System.out.println("Числа равны");}
-        //// Вторая задача
+            System.out.println("");
 
+        //// Вторая задача
+        double a = 3.4;
+        double b = 12;
+        double c = 64;
+        double Rootofdouble;
+        Rootofdouble = b * b - 4 * a * c;
+        if (Rootofdouble > 0) {
+            double x1, x2;
+            x1 = (-b - Math.sqrt(Rootofdouble)) / (2 * a);
+            x2 = (-b + Math.sqrt(Rootofdouble)) / (2 * a);
+            System.out.println("Корни уравнения: x1 = " + x1 + ", x2 = " + x2);
+        }
+        else if (Rootofdouble == 0) {
+            double x;
+            x = -b / (2 * a);
+            System.out.println("Уравнение имеет единственный корень: x = " + x);
+        }
+        else {
+            System.out.println("Уравнение не имеет действительных корней!");
+        }
+        System.out.println("");
 
         //// Третья задача
         int q = 643;
@@ -25,8 +47,44 @@ public class HW1 {
         }
         else {if (q2>q3) System.out.println(q2);
         else System.out.println(q3);}
+        System.out.println("");
 
-        ///
+        ///Четвертая задача
+
+        int num1 = 3;
+        int num2 = -4;
+        int num3 = 43;
+        int mid;
+
+
+
+        //// Цикл 1
+
+        int cir = 1000;
+        while (cir <= 9999){
+            System.out.println(cir);
+            cir+=3;
+        }
+        System.out.println("");
+
+        /// Цикл 2
+        for(int cir2 = 90; cir2>=0; cir2-=5){
+            System.out.println(cir2);
+        }
+        System.out.println("");
+
+        /// Цикл 3
+        int cir3 = 2;
+        for (int i = 0; i <20; i++){
+            System.out.println(cir3);
+            cir3 = cir3*2;
+        }
+        System.out.println("");
+
+        /// Цикл 4
+
+
+
 
 
 
