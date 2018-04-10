57ec048d1e44338374ca8db7ea76e800afca61ab
Dima <dimilian92@gmail.com>
2018-04-10 23:54:00 +0300

    Задание по "Войне и Миру"

---
+++ src/WarPeace.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class WarPeace {
+    public static void main(String[] args) throws IOException {
+        File wp = new File("src\\wp.txt");
+        List<String> afterWP = Files.readAllLines(wp.toPath());
+        List<String[]> list1 = new ArrayList<>();
+        List<String> arrLst = new ArrayList<>();
+        for (String line : afterWP) {
+            String[] wordSplit =
+                    line.replaceAll("[-.?)(,:;'\"*]", " ")
+                            .toLowerCase()
+                            .split("\\s");
+
+            for (String s : wordSplit) {
+                if (s.length() > 0)
+                    arrLst.add(s.trim());
+            }
+        }
+
+        for (String word : arrLst) {
+            System.out.println(word);
+        }
+
+
+        ////////// сколько раз встречается слово
+
+
+        HashMap<String, Integer> wordToCount = new HashMap<>();
+        for (String word : arrLst)
+        {
+            if (!wordToCount.containsKey(word))
+            {
+                wordToCount.put(word, 0);
+            }
+            wordToCount.put(word, wordToCount.get(word) + 1);
+        }
+        for (String word : wordToCount.keySet())
+        {
+            System.out.println(word + " " + wordToCount.get(word));
+        }
+    }
+}
