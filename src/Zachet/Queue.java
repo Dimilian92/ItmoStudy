5d48c243489d93456098faf269dd29285fef2f3b
Dima <dimilian92@gmail.com>
2018-04-16 02:17:58 +0300

    задание на зачет, пока без стака

--- src/Zachet/Queue.java
+++ src/Zachet/Queue.java
@@ -3,39 +3,41 @@ package Zachet;
 
 public class Queue<T> {
     private Node1 head;
-    public boolean add(T value){
+
+    public boolean add(T value) {
         head = new Node1(value);
-        if(head != null){
+        if (head != null) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
 
-}
+    }
 
 
-class Node1<T>{
-    private int size = 0;
-    private Node1 first = null;
-    private Node1 last = null;
-    T value;
+    class Node1<T> {
+        private int size = 0;
+        private Node1 first = null;
+        private Node1 last = null;
+        T value;
 
-    private Node1 next;
-    private Node1 prev;
+        private Node1 next;
+        private Node1 prev;
 
-    Node1(T element) {
-        this.value = value;
-        this.prev = last;
-        this.next = null;
-        if (last != null){
-            last.next = this;
+        Node1(T element) {
+            this.value = value;
+            this.prev = last;
+            this.next = null;
+            if (last != null) {
+                last.next = this;
+            }
+            if (first == null) {
+                first = this;
+            }
+            last = this;
+            size += 1;
         }
-        if (first == null){
-            first = this;
-        }
-        last = this;
-        size += 1;
     }
 }
+
 
